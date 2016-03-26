08a95e2c56678fd938d636e126dc9108411ebd8b
Sebastian Bauer <mail@sebastianbauer.info>
2016-03-26 14:59:20 +0100

    Refactored. Scanning and building xml is now separate.

--- update-maven-metadata.py
+++ update-maven-metadata.py
@@ -26,40 +26,42 @@ def ids(path):
 
 utcnow = datetime.datetime.utcnow
 
-# The following is keyed by group_id
-metadata = {}
-versioning = {}
-versions = {}
+# Groups contain artifacts that contain version that contains released jars
+groups = {}
 
-#group_id = None
-#last_group_id = None
-
+# Scan directory and extract jars and where they belong to
 for root, dir, files in os.walk("."):
 	if '.git' in dir:
 		# Don't enter the .git directory
 		dir.remove('.git')
 		continue
 
-	for file in files:
-		if file == 'maven-metadata.xml':
-			e = ET.parse(os.path.join(root, file)).getroot()
-			print(root)
-		elif file.endswith('.jar'):
-			version = os.path.basename(root)
-			other = os.path.dirname(root)
-			group_id, artifact_id = ids(other)
+	for file in [f for f in files if f.endswith('.jar')]:
+		version = os.path.basename(root)
+		other = os.path.dirname(root)
+		group_id, artifact_id = ids(other)
 
-			if group_id not in metadata:
-				mt = ET.Element("metadata")
-				metadata[group_id] = mt
-				ET.SubElement(mt,"groupId").text = group_id
-				ET.SubElement(mt,"artifactId").text = artifact_id
-				versioning[group_id] = ET.SubElement(mt,"versioning")
-				versions[group_id] = ET.SubElement(versioning[group_id],"versions")
-				ET.SubElement(versioning[group_id], "lastUpdated").text = utcnow().strftime("%Y%m%d%H%M%S")
+		if group_id not in groups:
+			groups[group_id] = {}
 
-			ET.SubElement(versions[group_id],"version").text = version
+		if artifact_id not in groups[group_id]:
+			groups[group_id][artifact_id] = {}
 
-for group_id in metadata:
-		# Now pretty print if we have populated metadata
-		print(minidom.parseString(ET.tostring(metadata[group_id])).toprettyxml(indent="  "), end="")
+		if version not in groups[group_id][artifact_id]:
+			groups[group_id][artifact_id][version] = []
+
+		groups[group_id][artifact_id][version].append(file)
+
+# Build XML
+for group_id, artifacts in groups.iteritems():
+	for artifact_id, artifact_versions in artifacts.iteritems():
+		mt = ET.Element("metadata")
+		ET.SubElement(mt,"groupId").text = group_id
+		ET.SubElement(mt,"artifactId").text = artifact_id
+		versioning = ET.SubElement(mt,"versioning")
+		versions = ET.SubElement(versioning,"versions")
+		for version in artifact_versions:
+			ET.SubElement(versions,"version").text = version
+		ET.SubElement(versioning, "lastUpdated").text = utcnow().strftime("%Y%m%d%H%M%S")
+
+		print(minidom.parseString(ET.tostring(mt)).toprettyxml(indent="  "), end="")
